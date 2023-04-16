bb8278e07ae2b86ecab47454d4f020b3f3a84bdd
Barryk112 <barrykiely112@gmail.com>
2023-04-16 11:32:47 +0000

    Updated base.html template to have a navbar and footer

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidenav inialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
