2383a66d5f082488c2a30c541619232913a4c138
Baz <111312411+Barryk112@users.noreply.github.com>
2023-04-18 17:36:35 +0000

    fixed bug that caused my code not to read in my local css and js files, static folder needed to be inside taskmanagergit add .git add .

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Sidenav inialization
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
